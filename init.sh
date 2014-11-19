5b82a6eddacc33c6493e389e9c814d661c6989ea
Eric Ortego <eric@opelousas.org>
2014-11-19 16:27:05 -0600

    add interactive

--- init.sh
+++ init.sh
@@ -6,5 +6,5 @@ MYINIT="git-init"
 sudo apt-get install git
 sudo git clone https://github.com/redog/git-init
 
-sudo ${MYINIT}/configure.sh
+python ${MYINIT}/configure.py
 #sudo ${MYINIT}/authtoken.py
