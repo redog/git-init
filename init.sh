1b7e08ada47975caab8b0dfa4811e781c8e6d334
Eric Ortego <eric@opelousas.org>
2015-01-01 20:03:26 +0000

    i still dont git it

--- init.sh
+++ init.sh
@@ -3,13 +3,8 @@
 #sudo wget this?
 mkdir ~/tmp
 MYINIT="git-init"
-<<<<<<< HEAD
 sudo apt-get install -y git
 sudo git clone https://github.com/redog/git-init
-=======
-sudo apt-get install git
-git clone https://github.com/redog/git-init
->>>>>>> 97b1b4a54a7435f91922e172ff336e35da622bb9
 
 sudo apt-get install -y vcsh
 vcsh -f clone  https://github.com/redog/rc.git vim
