d44cf6b2ad0af2a85ee35233fd9e40e529af17c7
Eric Ortego <eric@opelousas.org>
2015-01-01 20:01:33 +0000

    deleting node180

--- init.sh
+++ init.sh
@@ -3,8 +3,17 @@
 #sudo wget this?
 mkdir ~/tmp
 MYINIT="git-init"
-sudo apt-get install git
+sudo apt-get install -y git
 sudo git clone https://github.com/redog/git-init
+
+sudo apt-get install -y vcsh
+vcsh -f clone  https://github.com/redog/rc.git vim
+vcsh vim reset --hard origin/master
+sudo apt-get intall -y awscli
+sudo apt-get intall -y fabric
+sudo apt-get intall -y vim
+sudo apt-get intall -y boto
+
 
 # configure git 
 #python ${MYINIT}/configure.py
