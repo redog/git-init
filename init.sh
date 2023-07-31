53ddee5ae656444db50fb47af9cc80cf0779f7a8
Dirty REdOG <redog@users.noreply.github.com>
2023-07-31 13:48:54 -0500

    updates

--- init.sh
+++ init.sh
@@ -2,19 +2,47 @@
 
 MYINIT="git-init"
 
+choose() {
+  options=("$@")
+  for i in "${!options[@]}"; do
+    echo "[$i] ${options[i]}" >&2
+  done
+
+  while true; do
+    read -p "Please select: " choice
+    if [[ $choice =~ ^[0-9]+$ ]] && [[ $choice -ge 0 ]] && [[ $choice -lt ${#options[@]} ]]; then
+      echo "${options[$choice]}"
+      return
+    else
+      echo "That was not a valid choice!" >&2
+    fi
+  done
+}
+
+get_repositories() {
+  token="$1"
+  username="$2"
+  curl -H "Authorization: token ${token}" "https://api.github.com/user/repos" 2>&1 | \
+    grep -o '"full_name":\s*"[^"]*"' | \
+    sed -E 's/"full_name":\s*"([^"]*)"/\1/'
+}
+
 # Attempt to retrieve the password
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
   pass=$(security find-generic-password -s 'github' -w 2>/dev/null)
   if [[ $? -ne 0 ]]; then
-    echo "Failed to retrieve GitHub password"
+    echo "Failed to retrieve GitHub access token"
     exit 1
   fi
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
+  pass=$(secret-tool lookup github accesstoken 2>/dev/null)
   # Replace this with the actual command for Linux
-  echo "Linux not yet supported"
-  exit 1
+  if [[ $? -ne 0  ]]; then
+    echo "Problem retrieving GitHub access token"
+    exit 1
+  fi
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
   # Windows
   # Replace this with the actual command for Windows
@@ -26,6 +54,27 @@ else
   exit 1
 fi
 
-git clone https://github.com/redog/$MYINIT
+gitusername=$(git config user.github.login.name)
+
+if [[ $? -ne 0 ]]; then
+  echo "Github login name not set. Configure with git. "
+  exit 1
+fi
+
 export GITHUB_ACCESS_TOKEN="$pass"
-python3 ${MYINIT}/mkrepo.py
+
+echo ""
+echo "What would you like to do?"
+choose "Create a new repository" "Clone an existing repository"
+
+
+if [[ $choice -eq 0 ]]; then
+  python3 ${MYINIT}/mkrepo.py
+else 
+  repos=$(get_repositories "${GITHUB_ACCESS_TOKEN}")
+  IFS=$'\n' read -rd '' -a repo_array <<<"$repos"
+  chosen_repo=$(choose ${repo_array[@]})
+  git clone https://github.com/${chosen_repo}
+  git -C "./${chosen_repo#*/}" config "user.github.token" "${pass}"
+#python3 ${MYINIT}/mkrepo.py
+fi
